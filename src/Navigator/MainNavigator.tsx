ea99dfa9fdf5478e13c8a7f6923d98daefdd2a77
ShahidPathan09 <shahid_pathan@apprely.com>
2023-05-27 04:30:30 +0530

    Main Navigator

--- src/Navigator/MainNavigator.tsx
+++ src/Navigator/MainNavigator.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Home from '../Container/Home';
+import UpdateData from '../Container/UpdateData';
 
 function MainNavigator() {
+  const Stack = createStackNavigator();
+
   return (
-    <>
-      <div>MainNavigator</div>
-    </>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="UpdateData" component={UpdateData} />
+    </Stack.Navigator>
   );
 }
 
