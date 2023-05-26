5a713764b0b0fc73906170dc48ceb54610ed7ffb
ShahidPathan09 <shahid_pathan@apprely.com>
2023-05-27 04:29:46 +0530

    Initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community'],
+  rules: {
+    'array-bracket-spacing': ['error', 'never'],
+    'react/require-default-props': ['error'],
+    'react/default-props-match-prop-types': ['error'],
+    'react/sort-prop-types': ['error'],
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
 };
